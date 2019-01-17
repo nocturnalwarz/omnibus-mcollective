6a7e70a4030dd6408a035d36d7915dc936043fb7
Jason Miller <jason.miller@fidelitylife.com>
2019-01-17 10:33:59 -0600

    Updating mcollective-rest version

--- config/software/mcollective-rest-agent.rb
+++ config/software/mcollective-rest-agent.rb
@@ -1,11 +1,12 @@
 name "mcollective-rest-agent"
-default_version "0.0.3"
+default_version "0.0.4"
 
 dependency "mcollective"
 
 version("0.0.1") { source md5: '6a5b410c7e6568fbc0ad68bedeef716a' }
 version("0.0.2") { source md5: '9703623aa9376e74316ba12b2487bf52' }
 version("0.0.3") { source md5: 'ae93e65a5f60e0928e2712cd0ec7fcd2' }
+version("0.0.4") { source md5: 'c65da8c6f0b9d9bf872e2a086f590d58' }
 
 source url: "https://github.com/nocturnalwarz/mcollective-rest/archive/#{version}.tar.gz"
 
